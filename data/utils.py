5d723753ae28cfd7d9f7defe13702b71044b03e6
SarahLiaw <sarah.liaw8@gmail.com>
2025-05-17 20:27:03 -0700

    Update with k

--- data/utils.py
+++ data/utils.py
@@ -1,16 +1,12 @@
 import numpy as np
 
 def add_noise(data, noisy_percent):
-    if noisy_percent == 0:
-        return data
-
-    noisy_data = np.copy(data)
-    for i in range(data.shape[0]):
-        signal_std = np.std(data[i, :])
-        noise_std = noisy_percent * signal_std
-        noise = np.random.normal(0, noise_std, data.shape[1])
-        noisy_data[i, :] = data[i, :] + noise
-    return noisy_data
+    # if noisy percent is 0, then just return the data
+    # if not, just make a copy of the noisy data based on 
+    # the standard deviation of the data
+    # and add noise to the data (ie take the std of the data and multiply it by the noisy percent)
+    # and add that to the data
+    # for each row in the data
 
 
 def get_partial_observations(data, fraction_observed, seed=None):
